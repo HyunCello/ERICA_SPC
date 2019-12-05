72574c5dd7882286ef28cf1b464dfcfc0ab15100
hsoo38444 <hsoo3844@gmail.com>
2019-12-05 09:31:35 +0900

    js파일 분리

---
+++ script/main.js
@@ -0,0 +1,6 @@
+function openNav(){  /*style의 크기를 30%로 변경*/
+    document.getElementById("myTopnav").style.height = "30%";
+}
+function closeNav(){ /*style의 크기를 0으로 변경*/
+    document.getElementById("myTopnav").style.height = "0";
+}
