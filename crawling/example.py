546212b5473f8eb5ab92ee222482204d3d0e4e01
HyunCello <jeremykim95@gmail.com>
2019-12-22 21:17:41 +0900

    readme 수정

--- crawling/example.py
+++ crawling/example.py
@@ -3,9 +3,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://policy.nec.go.kr/")
+html = urlopen("https://www.naver.com/")
 bsObject = BeautifulSoup(html, "html.parser")
-f = open("crawlled/제19대대통령후보전체?.txt", 'w')
+f = open("crawlled/네이버.txt", 'w')
 for link in bsObject.find_all():
     data = link.text.strip()+"\n"
     f.write(data)
