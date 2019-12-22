8237983ff71eb5cd6e4fea96756fd23f2d765cb7
HyunCello <jeremykim95@gmail.com>
2019-12-22 19:53:52 +0900

    readme 수정 및 여러 코드 수정

--- crawling/example.py
+++ crawling/example.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*- 
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://policy.nec.go.kr/svc/policy/PolicyContent119.do")
+html = urlopen("http://policy.nec.go.kr/")
 bsObject = BeautifulSoup(html, "html.parser")
-f = open("/home/yh/git/SPC/crawling/crawlled/제19대대통령후보전체.txt", 'w')
+f = open("crawlled/제19대대통령후보전체?.txt", 'w')
 for link in bsObject.find_all():
     data = link.text.strip()+"\n"
     f.write(data)
